79c7ba5696365fa4e1ebcc41e5b4c9f50703365b
Mark <belyackovmark508@gmail.com>
2025-11-23 18:38:19 +0300

    videos

--- DjangoWebProject2/DjangoWebProject2/settings.py
+++ DjangoWebProject2/DjangoWebProject2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '15180109-3680-4b34-939d-256a5b83ebeb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
